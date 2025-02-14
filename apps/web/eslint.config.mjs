137b3a61e0303134ed8f111ab9379f10c7c4a83f
Alex Fischman <afischman710@gmail.com>
2025-02-14 13:16:52 -0600

    Fixing lint config

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -2,11 +2,32 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    // Apply to all JavaScript/JSX files
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      // Specify global variables for React and Jest
+      globals: {
+        ...globals.browser,
+        React: "readonly",
+        test: "readonly",
+        expect: "readonly",
+      },
+    },
+    settings: {
+      // Automatically detect the React version
+      react: {
+        version: "detect",
+      },
+    },
+    // Extend the recommended configurations
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {},
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
